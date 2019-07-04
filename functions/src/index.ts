5641f1de4dd56ed015113242a76d73a3564debb8
Audun Bjørnerud Mo <audun.bjornerud.mo@finn.no>
2019-07-04 15:49:12 +0200

    fix: linted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
-import * as functions from 'firebase-functions';
-import {Region} from '../@types/types'
-import {scrapeRegions, parseTeamsFromRegion} from './scraper'
+import * as functions from 'firebase-functions'
+import consola from 'consola'
+import { scrapeRegions, parseTeamsFromRegion } from './scraper'
+import { Region } from '../@types/types'
+
+consola.wrapAll()
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -11,25 +14,24 @@ import {scrapeRegions, parseTeamsFromRegion} from './scraper'
 
 export const scrapeTeams = functions.pubsub
   .schedule('0 0 * * *')
-  .onRun((context) => {
-    console.info(`Scrape requested by: ${context.auth ? context.auth.uid : 'UNKNOWN USER'}`)
+  .onRun(context => {
+    console.info(
+      `Scrape requested by: ${context.auth ? context.auth.uid : 'UNKNOWN USER'}`
+    )
     console.info(`Scrape requested at ${context.timestamp}`)
 
-    if(!context.auth) {
+    if (!context.auth) {
       console.error('User not authenticated / unknownuser. Skipping scrape')
       return
     }
 
-    const urls = [
-      'https://lol.gamepedia.com/Category:North_American_Teams'
-    ]
+    const urls = ['https://lol.gamepedia.com/Category:North_American_Teams']
     console.info(`Scraping ${urls.length} base urls`)
 
-    scrapeRegions(urls)
-      .then(responses => {
-        responses.map(response=> {
-          const team = parseTeamsFromRegion(response.data, Region.NA)
-          console.log(team)
-        })
+    scrapeRegions(urls).then(responses => {
+      responses.map(response => {
+        const team = parseTeamsFromRegion(response.data, Region.NA)
+        console.log(team)
       })
+    })
   })

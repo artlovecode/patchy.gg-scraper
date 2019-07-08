94de8c4754c252c2c7540fc252b7f90b2be6db3c
Audun Bjørnerud Mo <audun.bjornerud.mo@finn.no>
2019-07-08 21:38:16 +0200

    feat: init player scraping

--- src/regions.ts
+++ src/regions.ts
@@ -4,13 +4,13 @@ import { Region } from '../@types/types'
 
 export const handler: Handler = async (
   event: APIGatewayEvent,
-  context: Context,
-  callback: Callback
+  context: Context
 ) => {
   const regionHTMLs = await scrapeRegions(['https://lol.gamepedia.com/Category:North_American_Teams'])
   const teamsByRegions = regionHTMLs
     .map(response => response.data)
     .map(html => parseTeamsFromRegion(html, Region.NA))
+  
   return {
     body: JSON.stringify({
       data: teamsByRegions
